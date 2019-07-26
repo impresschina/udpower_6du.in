4bca2162b8132cfc56f6cdc073763a463b8b19ba
impresschina <impress-china@hotmail.com>
2019-07-26 14:24:57 +0800

    针对目前API接口提供的SDK的调用实例

--- demo.php
+++ demo.php
@@ -6,21 +6,33 @@
  * Time: 16:46
  */
 include_once './vendor/autoload.php';
-$appid = 'c81e728d9d4c2f636f067f89cc14862c';
-$accesskey = '80a1ca94-6d67-36d0-b624-5eb819c38dfa';
-$api = 'http://api.dove.io';//生产环境可以不用设置
 
-//$sdk = new \Sixdu\SixduSDK($appid, $accesskey);
-$sdk = new \Sixdu\SixduSDK($appid, $accesskey, $api);
-$urls = ['https://github.com/impresschina/sixdu/blob/master/src/SixduSDK.php', 'https://www.baidu.com/s?ie=UTF-8&wd=6du', 'https://www.baidu.com/s?ie=utf-8&f=3&rsv_bp=1&tn=baidu&wd=spport&oq=supper&rsv_pq=ea2254360003cc1d&rsv_t=9d89EiJp0kQuRu%2Fij9g9zCX5f393qh2m4xqDGXKvhQ%2BohUVBMgIa84UK4vo&rqlang=cn&rsv_enter=1&rsv_sug3=15&rsv_sug1=13&rsv_sug7=101&rsv_sug2=0&prefixsug=sppo&rsp=0&rsv_dl=ts_0&inputT=10445&rsv_sug4=11260'];
-$response = $sdk->make($urls);
+const SecretKey = '54a4be329214c553ICAgICA4454d25898be4ad7gMjU4Mg';
+$api = new \Sixdu\API(SecretKey);
 
-var_dump($response->success, $response->status_code);
-print_r(json_decode($response->body));
+try {
+    $body = $api->add('http://baidu.com/abc?a=1000');
+    print_r($body);
+    echo "\n";
 
-//for xml to array
-/*try {
-    print_r($sdk->xml2array($response->body));
-} catch (Exception $e) {
-    print_r($e->getTraceAsString());
-}*/
+    $short = 'http://wz4.in/1z56g';
+    $body = $api->parse($short);
+    print_r($body);
+    echo "\n";
+
+    $all = $api->statistics->all($short, date('Y-m-d'));
+    print_r($all);
+    echo "\n";
+
+    $one = $api->statistics->one($short);
+    print_r($one);
+    echo "\n";
+
+    $ip = $api->statistics->ip('120.85.77.233');
+    print_r($ip);
+    echo "\n";
+
+} catch (ErrorException $e) {
+    echo $e->getMessage() . "\n" . $e->getTraceAsString();
+    echo "\n";
+}
