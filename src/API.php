f78d58a36816d0094fba5ec2c419a2583ea1ed3c
impresschina <impress-china@hotmail.com>
2019-07-26 14:23:15 +0800

    针对目前API接口提供的SDK

---
+++ src/API.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ken
+ * Date: 2019-07-26
+ * Time: 09:39
+ */
+
+namespace Sixdu;
+
+/**
+ * Class API
+ * @property  Statistics $statistics
+ * @package Sixdu
+ */
+class API
+{
+    /**
+     * API URL
+     */
+    const API = 'http://xapi.in';
+
+    /**
+     * @var Statistics
+     */
+    private static $_statistics;
+
+    /**
+     * @var
+     */
+    private $query;
+
+    /**
+     * SixduAPI constructor.
+     * @param $secretKey
+     */
+    public function __construct($secretKey)
+    {
+        $this->query['secretkey'] = &$secretKey;
+    }
+
+    /**
+     * @param string $path
+     * @param array $query
+     * @return string
+     */
+    public function builder($path, $query)
+    {
+        return self::API . $path . '?' . http_build_query($this->query + $query);
+    }
+
+    /** 创建短网址请求
+     * @param string $url 将要生成的短网址的网址
+     * @return array
+     * @throws \ErrorException
+     */
+    public function add($url)
+    {
+        $request = \Requests::get($this->builder('/urls/add', ['lurl' => $url]));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return array('status' => 1, 'url' => $request->body);
+    }
+
+    /**
+     * 解析短网址请求
+     * @param string $url 已生成的短网址
+     * @return array
+     * @throws \ErrorException
+     */
+    public function parse($url)
+    {
+        $request = \Requests::get($this->builder('/urls/parse', ['surl' => $url]));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return array('status' => 1, 'url' => $request->body);
+    }
+
+    /**
+     * @param $name
+     * @return Statistics
+     */
+    public function __get($name)
+    {
+        if ($name == 'statistics') {
+            if (is_null(static::$_statistics))
+                static::$_statistics = new Statistics($this);
+            return static::$_statistics;
+        }
+        return null;
+    }
+}
---
+++ src/Statistics.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ken
+ * Date: 2019-07-26
+ * Time: 12:02
+ */
+
+namespace Sixdu;
+
+class Statistics
+{
+    /**
+     * @var API
+     */
+    private $api;
+
+    /**
+     * Statistics constructor.
+     * @param $api
+     */
+    public function __construct($api)
+    {
+        $this->api = &$api;
+    }
+
+    /**
+     * 查看具体日期统计报表
+     * @param string $url 短网址
+     * @return bool|mixed
+     * @throws \ErrorException
+     */
+    public function one($url)
+    {
+        $parser = \Spatie\Url\Url::fromString($url);
+
+        $request = \Requests::get($this->api->builder('/urls/one', array('code' => ltrim($parser->getPath(), "/"))));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return false;
+
+    }
+
+    /**
+     * 查看所有统计报表
+     * @param string $url 短网址
+     * @param $begin $begin 开始统计的日期，如 2019-06-01
+     * @return array|false
+     * @throws \ErrorException
+     */
+    public function all($url, $begin)
+    {
+        $parser = \Spatie\Url\Url::fromString($url);
+
+        $time = strtotime($begin);
+        if ($time === false || strtotime(date('Y-m-d')) > $time) {
+            throw new \ErrorException('Please set the correct begin date parameter');
+        }
+
+        $request = \Requests::get($this->api->builder('/urls/all', array('code' => ltrim($parser->getPath(), "/"), 'start_day' => $begin)));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return false;
+    }
+
+    /**
+     * 查询单一短网址7天访问详情
+     * @param string $url 短网址
+     * @return bool|mixed
+     * @throws \ErrorException
+     */
+    public function record($url)
+    {
+        $parser = \Spatie\Url\Url::fromString($url);
+
+        $request = \Requests::get($this->api->builder('/urls/record', array('code' => ltrim($parser->getPath(), "/"))));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return false;
+
+    }
+
+    /**
+     * 查询IP地址的信息，返回IP地址的国家，省份，城市，公司／单位信息，ISP接入商信息（ipip.net同步更新）
+     * @param $ip
+     * @return bool|mixed
+     * @throws \ErrorException
+     */
+    public function ip($ip)
+    {
+        if (!filter_var($ip, FILTER_VALIDATE_IP))
+            throw new \ErrorException('Please set the correct ip address');
+
+        $request = \Requests::get($this->api->builder('/ip/find', array('ip' => $ip)));
+        $code = $request->status_code;
+        if ($code != 200)
+            throw new \ErrorException('Request has an error HTTP status code: ' . $code);
+
+        $data = json_decode($request->body, true);
+        if (is_array($data))
+            return $data;
+
+        return false;
+    }
+}
