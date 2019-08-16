9645d6829932a4603198515c5846710cc2a82d8e
impresschina <impress-china@hotmail.com>
2019-08-16 16:44:51 +0800

    修改了api接口地址

--- demo_v1.php
+++ demo_v1.php
@@ -24,18 +24,6 @@ try {
     print_r($body);
     echo "\n";
 
-    //修改短网址
-    $url = 'https://news.sina.com.cn/c/2019-07-30/doc-ihytcerm7395417.shtml';
-    $body = $api->alter('http://6du.in/0p66kkf', $url);
-    print_r($body);
-    echo "\n";
-
-    //统计相关 详细请查看接口文档
-    $data = $api->statistics('1003', '120.85.78.109');//查询ip的信息
-    $data = $api->statistics('1000', 'http://6du.in/1ob1N5i');//该短网址最近访问记录统计
-    $data = $api->statistics('1001', 'http://6du.in/1ob1N5i');//详细统计
-    $data = $api->statistics('1002', 'http://6du.in/1ob1N5i');//详细统计版本2
-
 } catch (ErrorException $e) {
     echo $e->getMessage() . "\n" . $e->getTraceAsString();
     echo "\n";
